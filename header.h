af1c4792984db2af8d224650bdbb98ef5d4dd8b2
anil sharma <anilsharmaec@gmail.com>
2016-11-17 17:57:43 +0530

    Add files via upload

---
+++ header.h
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/time.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<sys/sem.h>
+#include<unistd.h>
+#include<string.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<time.h>
+#include<pthread.h>
+#include<dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdio_ext.h>
